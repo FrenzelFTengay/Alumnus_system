588b1b5e030623f79973be95aa2fe02d7bf88d30
Cla-Ir <claircomafay07@gmail.com>
2025-10-17 10:13:47 +0800

    Add files via upload

---
+++ database/migrations/create_ Alumni_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alumni', function (Blueprint $table) {
+            $table->id('AlumniId'); // Primary Key
+            $table->string('FullName');
+            $table->year('GraduationYear');
+            $table->string('Section')->nullable();
+            $table->string('Email')->unique();
+            $table->string('Password');
+            $table->string('ContactNumber')->nullable();
+            $table->string('CurrentOccupationEducation')->nullable();
+            $table->string('Status')->default('active');
+
+            // Foreign Keys
+            $table->unsignedBigInteger('SchoolId')->nullable();
+            $table->unsignedBigInteger('AnnouncementId')->nullable();
+            $table->unsignedBigInteger('YearId')->nullable();
+            $table->unsignedBigInteger('BatchId')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alumni');
+    }
+};
